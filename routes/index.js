43ddd94141825c97870c2f90fe5d1155774bc3c2
Jason <bboylilshaw@gmail.com>
2014-03-03 22:54:33 +0800

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,30 @@
-/*
- * GET home page.
- */
+var db = require('mongoose');
+var URLModel = db.model('URL');
 
 exports.index = function (req, res) {
     res.render('index', { title: 'Express' });
 };
 
 exports.shorten = function (req, res) {
-    res.write('url shortened');
+
+    var originalUrl = req.body.originalUrl;
+    var shortUrl = originalUrl + '_short';
+
+    var url = new URLModel({
+        originalUrl: originalUrl,
+        shortUrl: shortUrl,
+        generateDate: new Date()
+    });
+
+    url.save(function (err, url, numberAffected) {
+        console.log("url.save() invoked");
+        if (!err) {
+            console.dir(url);
+            res.redirect('/');
+        } else {
+            console.dir(err);
+            // Error handling
+            res.redirect('/error');
+        }
+    });
 };
--- util/db.js
+++ util/db.js
@@ -1,2 +1,33 @@
+// Bring Mongoose into the app
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:password@ds037737.mongolab.com:37737/urlstore');
+
+// Build the connection string
+var dbURI = 'mongodb://admin:password@ds037737.mongolab.com:37737/urlstore';
+
+// Create the database connection
+mongoose.connect(dbURI);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
+require('../model/url');
